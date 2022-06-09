edc9e4cd31ef4e089fa3c091420606f8104e21ce
Jason Prasad <jasongprasad@gmail.com>
2022-06-09 11:09:00 +0100

    Add test for parsing of numbers

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -1,5 +1,14 @@
 from calpy.calculators import postfix
 
 
+def test_parsing_numbers():
+    assert postfix("3") == 3
+    assert postfix("3    2") == 2
+    assert postfix("3 2 1") == 1
+    assert postfix("     3") == 3
+    assert postfix("   1337  ") == 1337
+    # TODO: testing of negative numbers? dc does not support this
+
+
 def test_postfix():
     assert postfix("21 21 +") == 42
