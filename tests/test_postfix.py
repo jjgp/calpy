80de01b468705aa3cf366f02455a239e5cf22e64
Jason Prasad <jasongprasad@gmail.com>
2022-06-09 11:18:22 +0100

    Add test for invalid inputs

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -1,3 +1,5 @@
+import pytest
+
 from calpy.calculators import postfix
 
 
@@ -7,7 +9,18 @@ def test_parsing_numbers():
     assert postfix("3 2 1") == 1
     assert postfix("     3") == 3
     assert postfix("   1337  ") == 1337
-    # TODO: testing of negative numbers? dc does not support this
+    # TODO: support of negative numbers? dc does not support this.
+
+
+def test_invalid_inputs():
+    with pytest.raises(ValueError, match="Expression resulted in empty stack"):
+        postfix("")
+    with pytest.raises(ValueError, match="Expression contains unsupported character"):
+        postfix("3 (╯°□°)╯︵ ┻━┻ 4")
+    with pytest.raises(ValueError, match="Expression resulted in empty stack"):
+        postfix("+")
+    with pytest.raises(ValueError, match="Expression resulted in empty stack"):
+        postfix("21 +")
 
 
 def test_postfix():
