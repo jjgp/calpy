76e487a750fdb7a83ce2f81a6ad6be4e9c6fd619
Jason Prasad <jasongprasad@gmail.com>
2022-06-09 11:04:45 +0100

    Passing simple addition

--- src/calpy/calculators.py
+++ src/calpy/calculators.py
@@ -1,2 +1,14 @@
 def postfix(expr):
-    pass
+    stack = []
+    i = 0
+    while i < len(expr):
+        if expr[i].isdigit():
+            j = i
+            while j < len(expr) and expr[j].isdigit():
+                j += 1
+            stack.append(int(expr[i:j]))
+            i = j
+        elif expr[i] == "+":
+            stack.append(stack.pop() + stack.pop())
+        i += 1
+    return stack[-1]
