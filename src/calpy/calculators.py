551d593228a022210eddc56b4ea161b45f0ddc1c
Jason Prasad <jasongprasad@gmail.com>
2022-06-09 11:27:35 +0100

    Supporting some invalid cases

--- src/calpy/calculators.py
+++ src/calpy/calculators.py
@@ -1,18 +1,30 @@
 def postfix(expr):
-    stack = []
     i = 0
+    stack = []
+    supported_operators = {"+": lambda x, y: x + y}
+
     while i < len(expr):
-        if expr[i].isdigit():
+        c = expr[i]
+
+        if c.isdigit():
             j = i
             while j < len(expr) and expr[j].isdigit():
                 j += 1
             stack.append(int(expr[i:j]))
             i = j
-        elif expr[i] == "+":
-            stack.append(stack.pop() + stack.pop())
-        elif expr[i] == " ":
-            continue
+        elif c in supported_operators:
+            if len(stack) < 2:
+                raise ValueError("Expression resulted in empty stack")
+            y, x = stack.pop(), stack.pop()
+            result = supported_operators[c](x, y)
+            stack.append(result)
+        elif c == " ":
+            pass
         else:
             raise ValueError("Expression contains unsupported character")
         i += 1
+
+    if not stack:
+        raise ValueError("Expression resulted in empty stack")
+
     return stack[-1]
